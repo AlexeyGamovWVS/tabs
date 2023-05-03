4f9a9d40c70ba7d7a5a8e38cb79d09d534b496d7
Alexey Gamov <101826239+AlexeyGamovWVS@users.noreply.github.com>
2023-05-03 12:49:21 +0300

    Update index.js
    delete unnecessary arguments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { UI_CLASSES, UI_SELECTORS, UI_SELECTORS_ANOTHER } from "./utils.js";
+import { UI_SELECTORS, UI_SELECTORS_ANOTHER } from "./utils.js";
 import { Tabs } from "./tabs.js";
 
-const tabs = new Tabs(UI_SELECTORS, UI_CLASSES);
+const tabs = new Tabs(UI_SELECTORS);
 tabs.initTabs();
 
-const tabsAnother = new Tabs(UI_SELECTORS_ANOTHER, UI_CLASSES);
+const tabsAnother = new Tabs(UI_SELECTORS_ANOTHER);
 tabsAnother.initTabs();
